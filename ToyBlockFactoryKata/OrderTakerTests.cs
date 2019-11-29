b08c67de263d3d21bb0f0fb4092f99c27a4b86da
markpearl <markpearl@gmail.com>
2019-11-29 13:50:01 +1300

    Initial

---
+++ ToyBlockFactoryKata/OrderTakerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ToyBlockFactoryKata
+{
+    public class OrderTakerTests
+    {
+        [Fact]
+        public void ShouldDoSomething()
+        {
+            
+        }
+    }
+}
