5bb0357fc912302e06fa5e032af2b2fa3ab8d786
markpearl <markpearl@gmail.com>
2019-12-02 16:05:00 +1300

    Prompt for console input with tests

--- ToyBlockFactoryKata/OrderTakerTests.cs
+++ ToyBlockFactoryKata/OrderTakerTests.cs
@@ -1,14 +1,52 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace ToyBlockFactoryKata
 {
     public class OrderTakerTests
     {
+        private static string _s;
+
         [Fact]
-        public void ShouldDoSomething()
+        public  void GetName()
         {
-            
+            var sut = new OrderTaker();
+            using StringWriter sw1 = new StringWriter();
+            Console.SetOut(sw1);
+            var sw = sw1;
+            using StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}");
+            Console.SetIn(sr);
+            var result = sut.GetOrder();
+            var expected = "Please input your Name: " + Environment.NewLine;
+            Assert.Equal(expected, sw.ToString());
         }
+
+        private static void SetConsoleInputToReader(string s1)
+        {
+            using StringReader sr = new StringReader(s1);
+            Console.SetIn(sr);
+        }
+
+        private static StringWriter SetConsoleOutputToWriter()
+        {
+            using StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+            return sw;
+        }
+    }
+
+    public class OrderTaker
+    {
+        public Order GetOrder()
+        {
+            Console.WriteLine("Please input your Name: ");
+            var name = Console.ReadLine();
+            return null;
+        }
+    }
+
+    public class Order
+    {
     }
 }
