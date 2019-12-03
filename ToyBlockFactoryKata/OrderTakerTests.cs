565f4d1eb67e05b6e2d907b0b0b29c566873fd68
markpearl <markpearl@gmail.com>
2019-12-03 15:51:07 +1300

    Validate address input

--- ToyBlockFactoryKata/OrderTakerTests.cs
+++ ToyBlockFactoryKata/OrderTakerTests.cs
@@ -14,16 +14,21 @@ namespace ToyBlockFactoryKata
             var sut = new OrderTaker();
             using (StringWriter sw = new StringWriter())
             {
-                using StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}Bobby");
+                var consoleInput = new List<string>();
+                consoleInput.Add("Mark Pearl");
+                consoleInput.Add("1 Bob Avenue, Auckland");
+//                consoleInput.Add("Some Random Text");
+                
+                using StringReader sr = new StringReader(String.Join(Environment.NewLine,consoleInput));
                 Console.SetOut(sw);
                 Console.SetIn(sr);
                 var result = sut.GetOrder();
                 var consoleOutput = sw.ToString().Split(Environment.NewLine).ToList();
                 Assert.Equal("Please input your Name: ", consoleOutput[0]);
                 Assert.Equal("Please input your Address: ", consoleOutput[1]);
-//                Assert.Equal("Please input your Due Date: ", consoleOutput[2]);
-                Assert.Equal("Mark Pearl", result.Name);
-//                Assert.Equal("Mark Pearl", result.Address);
+                Assert.Equal("Please input your Due Date: ", consoleOutput[2]);
+//                Assert.Equal("Mark Pearl", result.Name);
+                Assert.Equal("1 Bob Avenue, Auckland", result.Address);
             }
         }
     }
