13dfdaf207297575cc13f1eeab634935e57ec5c9
markpearl <markpearl@gmail.com>
2019-12-03 15:22:19 +1300

    Test value of name

--- ToyBlockFactoryKata/OrderTakerTests.cs
+++ ToyBlockFactoryKata/OrderTakerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
@@ -9,30 +10,20 @@ namespace ToyBlockFactoryKata
         private static string _s;
 
         [Fact]
-        public  void GetName()
+        public  void PromptName()
         {
             var sut = new OrderTaker();
-            using StringWriter sw1 = new StringWriter();
-            Console.SetOut(sw1);
-            var sw = sw1;
-            using StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}");
-            Console.SetIn(sr);
-            var result = sut.GetOrder();
-            var expected = "Please input your Name: " + Environment.NewLine;
-            Assert.Equal(expected, sw.ToString());
-        }
-
-        private static void SetConsoleInputToReader(string s1)
-        {
-            using StringReader sr = new StringReader(s1);
-            Console.SetIn(sr);
-        }
-
-        private static StringWriter SetConsoleOutputToWriter()
-        {
-            using StringWriter sw = new StringWriter();
-            Console.SetOut(sw);
-            return sw;
+            using (StringWriter sw1 = new StringWriter())
+            {
+                using (StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}"))
+                {
+                    Console.SetOut(sw1);
+                    Console.SetIn(sr);
+                    var result = sut.GetOrder();
+                    Assert.Equal("Please input your Name: " + Environment.NewLine, sw1.ToString());
+                    Assert.Equal("Mark Pearl", result.Name);
+                }
+            }
         }
     }
 
@@ -42,11 +33,17 @@ namespace ToyBlockFactoryKata
         {
             Console.WriteLine("Please input your Name: ");
             var name = Console.ReadLine();
-            return null;
+            return new Order(name);
         }
     }
 
     public class Order
     {
+        public Order(string name)
+        {
+            Name = name;
+        }
+
+        public String Name { get; }
     }
 }
