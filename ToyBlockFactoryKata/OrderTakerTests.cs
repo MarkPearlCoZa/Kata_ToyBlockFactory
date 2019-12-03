dade9250fe8ba38ce3eea1e426e687f160a86cd2
markpearl <markpearl@gmail.com>
2019-12-03 15:35:08 +1300

    Check address prompt

--- ToyBlockFactoryKata/OrderTakerTests.cs
+++ ToyBlockFactoryKata/OrderTakerTests.cs
@@ -1,28 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace ToyBlockFactoryKata
 {
     public class OrderTakerTests
     {
-        private static string _s;
-
         [Fact]
-        public  void PromptName()
+        public  void PromptOrderDetails()
         {
             var sut = new OrderTaker();
-            using (StringWriter sw1 = new StringWriter())
+            using (StringWriter sw = new StringWriter())
             {
-                using (StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}"))
-                {
-                    Console.SetOut(sw1);
-                    Console.SetIn(sr);
-                    var result = sut.GetOrder();
-                    Assert.Equal("Please input your Name: " + Environment.NewLine, sw1.ToString());
-                    Assert.Equal("Mark Pearl", result.Name);
-                }
+                using StringReader sr = new StringReader($"Mark Pearl{Environment.NewLine}Bobby");
+                Console.SetOut(sw);
+                Console.SetIn(sr);
+                var result = sut.GetOrder();
+                var consoleOutput = sw.ToString().Split(Environment.NewLine).ToList();
+                Assert.Equal("Please input your Name: ", consoleOutput[0]);
+                Assert.Equal("Please input your Address: ", consoleOutput[1]);
+//                Assert.Equal("Please input your Due Date: ", consoleOutput[2]);
+                Assert.Equal("Mark Pearl", result.Name);
+//                Assert.Equal("Mark Pearl", result.Address);
             }
         }
     }
@@ -33,17 +34,21 @@ namespace ToyBlockFactoryKata
         {
             Console.WriteLine("Please input your Name: ");
             var name = Console.ReadLine();
-            return new Order(name);
+            Console.WriteLine("Please input your Address: ");
+            var address = Console.ReadLine();
+            return new Order(name, address);
         }
     }
 
     public class Order
     {
-        public Order(string name)
+        public Order(string name, string address)
         {
             Name = name;
+            Address = address;
         }
 
         public String Name { get; }
+        public string Address { get; }
     }
 }
