868908e1eb476121c0cb269d9c2aa5bccdcdfbe6
markpearl <markpearl@gmail.com>
2020-01-07 16:03:41 +1300

    Broken with multiple entry points

---
+++ ToyBlockFactoryKata/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ToyBlockFactoryKata
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+        } 
+    }
+}
